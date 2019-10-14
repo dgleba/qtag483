83ecb9d539c09d7949a0822a24167f0a7420ad9b
David Glebagit <dgleba@gmail.com>
2019-10-14 15:08:39 -0500

    specify.table.name

--- app/models/tbl_quality_issue.rb
+++ app/models/tbl_quality_issue.rb
@@ -1,2 +1,12 @@
 class TblQualityIssue < ApplicationRecord
+  
+  # we can specify prefix, no need when using with 'dbo.' 
+  # self.table_name_prefix = 'dbo.' 
+  # specify table name if it breaks Rails conventions
+  self.table_name  = 'TBLQualityIssues'
+  # specify primary key name if it's other than id
+  # self.primary_key = 'id_virtual_channel'
+  #default_scope { order('date_time DESC') }
+  #default_scope ->{ order('date_time DESC') }
+
 end
