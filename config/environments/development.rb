b10937e8c6abbd42cdca2379eda14cd2123cb7cb
David Glebagit <dgleba@gmail.com>
2019-10-13 20:34:09 -0500

    add devise 2

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,22 +13,18 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
-  # Run rails dev:cache to toggle caching.
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+  if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+      'Cache-Control' => 'public, max-age=172800'
     }
   else
     config.action_controller.perform_caching = false
 
     config.cache_store = :null_store
   end
-
-  # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
@@ -40,9 +36,6 @@ Rails.application.configure do
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
-
-  # Highlight code that triggered database queries in logs.
-  config.active_record.verbose_query_logs = true
 
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
@@ -58,4 +51,32 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  
+  
+  
+  config.action_mailer.delivery_method = :smtp
+  # SMTP settings for
+  #10.5.1.210  # MESG01.stackpole.ca
+  # works..
+  
+  config.action_mailer.smtp_settings = {
+   :address           => "mesg01.stackpole.ca"
+  }
+ 
+  # gmail worked, 2016-07-21_Thu_14.44-PM.
+  # or try smtp-mail.outlook.com
+  
+  # config.action_mailer.smtp_settings = {
+  #  :address              => "smtp.gmail.com",
+  #  :port                 => 587,
+  #  :user_name            => ENV['gmail_username'],
+  #  :password             => ENV['gmail_password'],
+  #  :authentication       => "plain",
+  # :enable_starttls_auto => true
+  # } 
+  
+  #for devise.. 2016-07-22_Fri_14.52-PM David Gleba
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }  
+  config.action_mailer.default_url_options = { host: '10.4.1.228', port: 3001 }  
+
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -8,7 +8,7 @@ Devise.setup do |config|
   # confirmation, reset password and unlock tokens in the database.
   # Devise will use the `secret_key_base` as its `secret_key`
   # by default. You can change it below and use your own secret key.
-  # config.secret_key = '6f59546807bfe7f537364a8d063ab1f5e4023a2b102943e2d73ed6983f1c1fbc574a84775981ab1fc4097aaf29d2e39e0ea09b2d5e923bf6fc8287085ad8ad84'
+   config.secret_key = '6f59546807bfe7f537364a8d063ab1f5e4023a2b102943e2d73ed6983f1c1fbc574a84775981ab1fc4097aaf29d2e39e0ea09b2d5e923bf6fc8287085ad8ad84'
 
   # ==> Controller configuration
   # Configure the parent class to the devise controllers.
@@ -166,12 +166,15 @@ Devise.setup do |config|
 
   # ==> Configuration for :validatable
   # Range for password length.
-  config.password_length = 6..128
+  config.password_length = 1..128  
+
 
   # Email regex used to validate email formats. It simply asserts that
   # one (and only one) @ exists in the given string. This is mainly
   # to give user feedback and not to assert the e-mail validity.
-  config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
+  # config.email_regexp = /\A[^@\s]+@[^@\s]+\z/ 
+config.email_regexp = /./ 
+
 
   # ==> Configuration for :timeoutable
   # The time you want to timeout the user session without activity. After this
