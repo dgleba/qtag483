9ed389f691078185b6046a28a9d6c8bf1ebae1e2
David Glebagit <dgleba@gmail.com>
2019-10-13 20:38:46 -0500

    seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 3017_01_01_010111) do
+ActiveRecord::Schema.define(version: 5016_12_30_223301) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -83,29 +83,8 @@ ActiveRecord::Schema.define(version: 3017_01_01_010111) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
-    t.string "name"
-    t.integer "role_id"
-    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["role_id"], name: "index_users_on_role_id"
-    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type '' for column 'current_sign_in_ip'
 
   create_table "versions", force: :cascade do |t|
     t.string "item_type", null: false
