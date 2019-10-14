94cf55907aaf64a2dd99f13bfcf73fa086d8bdc2
David Glebagit <dgleba@gmail.com>
2019-10-13 20:35:24 -0500

    paper_trail - 3. setup

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,4 +1,14 @@
 RailsAdmin.config do |config|
+
+  # == Devise ==
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
+
+  # == Cancan ==
+  config.authorize_with :cancan
+  
   config.total_columns_width = 9999999
 
   ### Popular gems integration
@@ -16,7 +26,7 @@ RailsAdmin.config do |config|
   # config.authorize_with :pundit
 
   ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
+   config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
 
   ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
 
@@ -36,7 +46,7 @@ RailsAdmin.config do |config|
     show_in_app
 
     ## With an audit adapter, you can add:
-    # history_index
-    # history_show
+     history_index
+     history_show
   end
 end
